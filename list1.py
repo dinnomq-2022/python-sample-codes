1261a33f61faae170626c8f8b22557016e8d8eeb
Dinno Collantes <109901599+dinnomq-2022@users.noreply.github.com>
2024-10-02 12:16:46 +0000

    list1 sample

---
+++ list1.py
@@ -0,0 +1,12 @@
+def count_containing(list, character):
+    '''
+    returns the number of items in list that contain
+    the given character
+    '''
+    count = 0
+    for item in list:
+        if character in item:
+            print(character,"exists in string", item)
+            count+=1
+    return count
+count_containing(["Dinno","Gino"],"in")
